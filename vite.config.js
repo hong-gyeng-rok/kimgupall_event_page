452e525691aa30b6f763be26ed690e7c2b71f546
hong-gyeng-rok <ghdrudfhrr87@gmail.com>
2025-12-02 21:23:33 +0900

    github pages adress 수정

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: command === "build" ? "/kimgupall98_React_Tailwind/" : "/",
+    base: "/",
   };
 });
